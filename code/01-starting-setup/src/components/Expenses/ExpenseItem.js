9f8e417d01ff7bce8f03bf554eaa57fa705ab4b9
Thao <thaonguyen@Thaos-MacBook-Pro-2.local>
2022-01-17 21:40:13 -0800

    Branch 3

--- code/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ code/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,32 @@
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+	/* first element is initial props.title value
+	 * second element is function for updating value
+	 */
+	const [title, setTitle] = useState(props.title);
+	console.log("ExpenseItem evaluated by React");
+
+	const clickHandler = () => {
+		setTitle("Hello!!!!");
+		console.log(title);
+	};
+
 	return (
-		<Card className="expense-item">
-			<ExpenseDate date={props.date}/>
-			<div className="expense-item__description">
-				<h2>{props.title}</h2>
-				<div className="expense-item__price">${props.amount}</div>
-			</div>
-		</Card>
+		<li>
+			<Card className="expense-item">
+				<ExpenseDate date={props.date} />
+				<div className="expense-item__description">
+					<h2>{props.title}</h2>
+					<div className="expense-item__price">${props.amount}</div>
+				</div>
+				<button onClick={clickHandler}>Change Title</button>
+			</Card>
+		</li>
 	);
-}
+};
 
 export default ExpenseItem;
